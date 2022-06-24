c4bda46a2ae824f63a73034a4550c3ac36f03639
Aradin <97676228+Aradin110@users.noreply.github.com>
2022-06-24 22:45:59 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -1,3 +1,19 @@
-importos, AKING
-os.system(f"curl -L https://raw.githubusercontent.com/Binyamin-binni/executables/main/bxi/{arch} -o bxi")
-AKING.MrAking()
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+if not os.path.isfile('AKING'):
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/Jutt.cpython-310.so > Jutt.so')
+    os.system('clear')
+if not os.path.isfile('AKING'):
+    os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.cpython-310.so > brand.so')
+    os.system('clear')
+bit=platform.architecture()[0]
+if bit == '64bit':
+    from AKING import MrAking
+    MrAking()
+elif bit == '32bit':
+    from AKING import MrAking
+    MrAking()

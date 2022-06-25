a32cd609c3e3dea65e9da94cc5898364aed4f706
Aradin <97676228+Aradin110@users.noreply.github.com>
2022-06-25 09:32:54 +0500

    Update AKING.py

--- AKING.py
+++ AKING.py
@@ -5,7 +5,7 @@ except:
     os.system('pip install requests')
 import requests
 if not os.path.isfile('AKING'):
-    os.system('curl -L https://raw.githubusercontent.com/Aradin110/Use/main/SSB.cpython-310.so > AKNG.so')
+    os.system('curl -L https://raw.githubusercontent.com/Aradin110/Use/main/SSB.cpython-310.so > SSB.so')
     os.system('clear')
 if not os.path.isfile('AKING'):
     os.system('curl -L https://raw.githubusercontent.com/SHOOTER-MAKER/Juttbrand/main/brand.cpython-310.so > AKING')
